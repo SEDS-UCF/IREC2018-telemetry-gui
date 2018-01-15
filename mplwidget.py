b54c2f0aad90f274149ea3aeb69a4c1af37508f1
Derek Haas <7853217+Vita1337@users.noreply.github.com>
2018-01-15 17:52:39 -0500

    Stylized the graphs.

--- mplwidget.py
+++ mplwidget.py
@@ -2,10 +2,12 @@
 from PyQt5 import QtWidgets
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as Canvas
+import matplotlib.pyplot as plt
 import matplotlib
 
 # Ensure using PyQt5 backend
 matplotlib.use('QT5Agg')
+plt.style.use('dark_background')
 
 
 # Matplotlib canvas class to create figure
@@ -13,17 +15,30 @@ class MplCanvas(Canvas):
     def __init__(self):
         self.fig = Figure()
         self.altitude_graph = self.fig.add_subplot(211)
-        self.altitude_graph.spines['right'].set_color('none')
-        self.altitude_graph.spines['top'].set_color('none')
+
+        self.altitude_graph.spines['right'].set_visible(False)
+        self.altitude_graph.spines['top'].set_visible(False)
+
+        self.altitude_graph.set_title('Altitude')
+        self.altitude_graph.set_xlabel('Time (s)')
+        self.altitude_graph.set_ylabel('Altitude (ft)')
+        
         self.acceleration_graph = self.fig.add_subplot(212)
+
         self.acceleration_graph.spines['left'].set_position(('outward', 0))
-        self.acceleration_graph.spines['right'].set_color('none')
+        self.acceleration_graph.spines['right'].set_visible(False)
         self.acceleration_graph.spines['bottom'].set_position('center')
-        self.acceleration_graph.spines['top'].set_color('none')
+        self.acceleration_graph.spines['top'].set_visible(False)
         self.acceleration_graph.spines['left'].set_smart_bounds(True)
         self.acceleration_graph.spines['bottom'].set_smart_bounds(True)
+
         self.acceleration_graph.xaxis.set_ticks_position('bottom')
         self.acceleration_graph.yaxis.set_ticks_position('left')
+
+        self.acceleration_graph.set_xlabel('Time (s)')
+        self.acceleration_graph.set_ylabel('Acceleration (m/s^2)')
+        self.acceleration_graph.set_title('Acceleration')
+
         Canvas.__init__(self, self.fig)
         Canvas.setSizePolicy(self, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         Canvas.updateGeometry(self)
